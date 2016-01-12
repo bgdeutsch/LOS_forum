7f0c5ef7448fe7b6321aba317556d20eddd9f8e5
Brian Deutsch <bgdeutsch@gmail.com>
2016-01-12 00:11:05 -0500

    Default avatar fixing.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -33,7 +33,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-    "/images/fallback" + [:thumb, "avatar.png"].compact.join('_')
+    "/assets/images/" + [:thumb, "avatar.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
