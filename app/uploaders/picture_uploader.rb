38df7c5d931c48c4062054d06e67cd66c77097b7
Brian Deutsch <bgdeutsch@gmail.com>
2016-01-12 00:32:14 -0500

    Default avatar fixing.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -31,9 +31,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path('images/' + ["avatar.png"].compact.join('_'))
+    ActionController::Base.helpers.asset_path(["avatar.png"].compact.join('_'))
   #
-    "/images/" + ["avatar.png"].compact.join('_')
+    # "/images/" + ["avatar.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
